5f65180e85237eb8dd3fcfcc14c005a79ee7acc4
Nur Mohammad <mohammadnur5480@gmail.com>
2024-04-03 14:19:16 +0600

    feat: added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Archivo } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/view/Navbar";
+import Footer from "@/components/view/Footer";
 
 const archivo = Archivo({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={archivo.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
